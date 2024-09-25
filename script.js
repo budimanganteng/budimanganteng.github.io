31ee1a9f9fe36b5ceab429418ab90450ab6ef712
budimanganteng <162068756+budimanganteng@users.noreply.github.com>
2024-09-25 21:32:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const mode = document.querySelector('.sun .fa-solid');
+mode.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.body.classList.toggle('blackmode');
+    mode.classList.toggle('fa-sun');
+    mode.classList.toggle('fa-moon');
+});
+
+// alert('Selamat datang di web bootcamp');
+
+// var test_again = true;
+// while(test_again === true){
+//     var masuk = prompt("Anda yakin ingin masuk ke web ini? (yakin/tidak): ");
+//     if(masuk === "yakin"||"Yakin"){
+//         if(confirm("Apakah Anda yakin ingin masuk ke web ini? ")){
+//             test_again = true;
+//             test_again = document.querySelector(html)
+//         } else {
+//             alert(" Anda belum yakin, silakan coba lagi.");
+//             test_again = false;
+//         }
+//     } else {
+//         alert(" Anda belum yakin, silakan coba lagi.");
+//     }
+// }
+
+// alert('Terima Kasih....');
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
